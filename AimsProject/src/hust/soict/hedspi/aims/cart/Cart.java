f8205ffd92ef7c7fe72470e3870754348ca6d1af
Nguyen Huy Hoang <huyhoang01032006@gmail.com>
2026-05-10 23:14:01 +0700

    update

--- AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/hedspi/aims/cart/Cart.java
@@ -1,5 +1,7 @@
 package hust.soict.hedspi.aims.cart;
 
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
 public class Cart {
     // Số lượng DVD tối đa cho phép trong giỏ hàng
     public static final int MAX_NUMBERS_ORDERED = 20;
@@ -10,41 +12,34 @@ public class Cart {
     // Số lượng DVD hiện có thực tế trong giỏ hàng
     private int qtyOrdered = 0;
 
-    // PHƯƠNG THỨC GỐC VÀ NẠP CHỒNG (MỤC 11 & 14)
-
     // 1. Thêm MỘT DVD vào giỏ hàng (Phương thức gốc)
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-        // Kiểm tra xem giỏ hàng đã đầy chưa
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
-            System.out.println("The disc has been added"); // Thông báo thành công
+            System.out.println("The disc has been added");
         } else {
-            System.out.println("The cart is almost full"); // Thông báo khi đầy [cite: 647]
+            System.out.println("The cart is almost full");
         }
     }
 
-    // 2. Thêm danh sách DVD sử dụng Varargs (Mục 14.1) [cite: 702]
-    // Cho phép truyền vào số lượng tham số tùy ý (ví dụ: addDigitalVideoDisc(dvd1, dvd2, dvd3))
+    // 2. Thêm danh sách DVD sử dụng Varargs
     public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
         for (DigitalVideoDisc disc : dvdList) {
-            addDigitalVideoDisc(disc); // Gọi lại phương thức gốc để tận dụng logic kiểm tra số lượng
+            addDigitalVideoDisc(disc);
         }
     }
 
-    // 3. Thêm chính xác 2 DVD vào giỏ hàng (Mục 14.2) [cite: 704, 706-708]
+    // 3. Thêm chính xác 2 DVD vào giỏ hàng
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-        addDigitalVideoDisc(dvd1); // Gọi lại phương thức gốc
-        addDigitalVideoDisc(dvd2); // Gọi lại phương thức gốc
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
     }
-
-    // CÁC PHƯƠNG THỨC KHÁC
 
     // Xóa một DVD khỏi giỏ hàng
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i] == disc) {
-                // Dịch chuyển các phần tử phía sau lên để lấp chỗ trống
                 for (int j = i; j < qtyOrdered - 1; j++) {
                     itemsOrdered[j] = itemsOrdered[j + 1];
                 }
@@ -57,25 +52,53 @@ public class Cart {
         System.out.println("The disc is not in the cart");
     }
 
-    // Tính tổng chi phí của các DVD trong giỏ
+    // Tính tổng chi phí
     public float totalCost() {
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
             total += itemsOrdered[i].getCost();
         }
-        return total; // Trả về tổng chi phí
+        return total;
     }
 
-    // Phương thức bổ sung để hiển thị danh sách các mục (Yêu cầu Mục 12)
+    // In danh sách giỏ hàng
     public void displayCart() {
         System.out.println("***********************CART***********************");
         System.out.println("Ordered Items:");
         for (int i = 0; i < qtyOrdered; i++) {
-            System.out.println((i + 1) + ". DVD - " + itemsOrdered[i].getTitle() +
-                    " - " + itemsOrdered[i].getCategory() +
-                    " : " + itemsOrdered[i].getCost() + " $");
+            // Gọi hàm toString() của DigitalVideoDisc để in theo định dạng chuẩn
+            System.out.println((i + 1) + ". DVD - " + itemsOrdered[i].toString());
         }
         System.out.println("Total Cost: " + totalCost() + " $");
         System.out.println("***************************************************");
+    }
+
+    // Tìm kiếm theo ID
+    public void searchById(int id) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println("Found match: " + itemsOrdered[i].toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for ID: " + id);
+        }
+    }
+
+    // Tìm kiếm theo Tiêu đề
+    public void searchByTitle(String title) {
+        boolean found = false;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println("Found match: " + itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("No match found for title: " + title);
+        }
     }
 }
--- AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -1,4 +1,30 @@
 package hust.soict.hedspi.test.cart;
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
 
 public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        // tao dvd de test
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
+
+        // them vao cart
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // in ra
+        cart.displayCart();
+
+        // Test tìm kiếm theo ID
+        cart.searchById(1); // Giả sử tìm DVD có ID là 1
+        cart.searchById(5); // Tìm ID không tồn tại
+
+        // Test tìm kiếm theo tiêu đề
+        cart.searchByTitle("Star Wars");
+        cart.searchByTitle("Harry Potter"); // Tìm title không tồn tại
+    }
 }
